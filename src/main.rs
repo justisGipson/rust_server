3118c630ff239430e3275524180db0d838866fd7
Justis <jgipson@codelicious.com>
2021-03-19 11:49:28 -0400

    simple single threaded server

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+// simple web server example. in less than 40 lines.
+// responds to one request with content
+// all other requests result in 404
+// runs in a single thread
+
+use std::io::prelude::*;
+use std::fs;
+use std::net::TcpListener;
+use std::net::TcpStream;
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let _stream = stream.unwrap();
+
+        handle_connection(_stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+
+    let get = b"GET / HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+
+    let response = format!("{}{}", status_line, contents);
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
