e83cfe8ea48be3cef9d4b7f6dd6d35f53c08f815
Justis <jgipson@codelicious.com>
2021-03-22 09:39:25 -0400

    fixed comment

--- src/lib.rs
+++ src/lib.rs
@@ -16,13 +16,13 @@ enum Message {
 }
 
 impl ThreadPool {
-    /// Create a new ThreadPool.
-    ///
-    /// The size is the number of threads in the pool.
-    ///
-    /// # Panics
-    ///
-    /// The `new` function will panic if the size is zero.
+    // Create a new ThreadPool.
+    //
+    // The size is the number of threads in the pool.
+    //
+    // # Panics
+    //
+    // The `new` function will panic if the size is zero.
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
 
